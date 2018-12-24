f05f29244e24b88a2c5f8f8c6ac0ed2e7a81174f
yuntaoyt <13683308013@163.com>
2018-12-24 18:58:09 +0800

    ss

--- springbootTest/src/main/java/com/taotao/controller/IndexController.java
+++ springbootTest/src/main/java/com/taotao/controller/IndexController.java
@@ -1,13 +1,21 @@
 package com.taotao.controller;
 
 import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.sf.ezmorph.bean.MorphDynaBean;
+import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 @Controller
 public class IndexController {
@@ -22,6 +30,32 @@ public class IndexController {
 		map2.put("name", "xian");
 		list.add(map1);
 		list.add(map2);
+		
+	
+		JSONArray li = JSONArray.fromObject(list);    //把array数组对象转换成 json
+	    System.out.println(li.toString());
+		
+		 List ters =(List)JSONArray.toCollection(li); //jsonarray 转 对象
+		 
+		 MorphDynaBean tts=(MorphDynaBean)ters.get(0);
+		 String name = (String) tts.get("name");
+		 System.out.println(name);
+		
+		
+		JSONObject ter  =JSONObject.fromObject(map1);   //把对象转换成  json
+	    System.out.println("json的使用"+ter.toString());
+	    
+	    Map map  =(Map)JSONObject.toBean(ter,Map.class);//把json转成对象 
+	    System.out.println(map);
+	    
+	    
+	    Map  maps = new HashMap<>();
+	    maps.put("list", list);
+	    
+	    JSONObject tes = JSONObject.fromObject(maps);    //对象换成json
+	    System.out.println(tes.toString());
+	    
+		
 		System.out.println(list);
 		model.addAttribute("list", list);
 		model.addAttribute("name","y");
