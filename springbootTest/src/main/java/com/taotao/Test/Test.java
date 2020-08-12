4d13ccdb4a3531c107fa57b892d3eef2b8a8dabc
yuntao <123456yuntao>
2020-08-12 11:48:53 +0800

    twta

--- springbootTest/src/main/java/com/taotao/Test/Test.java
+++ springbootTest/src/main/java/com/taotao/Test/Test.java
@@ -3,7 +3,5 @@ package com.taotao.Test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("yuntao");
-        System.out.println("测试");
-        System.out.println("cc");
     }
 }
